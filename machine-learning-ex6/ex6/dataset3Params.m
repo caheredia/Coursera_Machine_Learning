2aa0674858604a06521feefed364e2a1119ca066
caheredia <cristianheredia@gmail.com>
2017-12-05 18:44:00 -0800

    completed first two sections of assignment

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+error_check = inf;
 
+for C = [0.01 0.03 0.1 0.3 1 3 10 30]
+  for sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+     model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+     predictions = svmPredict(model, Xval);
+     error = mean(double(predictions ~= yval));
+     if error < error_check
+       C_final = C;
+       sigma_final = sigma;
+       error_check = error;
+     end 
+  end
+end 
 
-
-
+C=C_final;
+sigma=sigma_final;
 
 
 % =========================================================================
